03f0c7f9a8881f25aac7f05c8e01151190779cb5
shinya-takagi <shinya523@gmail.com>
2020-09-30 09:47:58 +0900

    anything

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+echo "generate readlink dotfiles? (y/v/*)"
+echo "in vostok, write v. others, not work."
+read g
+
+case $g in
+	"y" ) { ln -sf ~/dotfiles/.vimrc ~/.vimrc
+		ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+	};;
+	"v" ) { ln -sf ~/dotfiles/.vimrc_vos ~/vimrc
+		ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+	};;
+	* )	echo "stop"
+esac
 
 exit
 
