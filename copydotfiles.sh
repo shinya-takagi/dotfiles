23bfe89cd549dfad77717a58e66755b8e7692634
shinya takagi <nori@MacAKASAKA.local>
2020-12-13 19:56:32 +0900

    anything

--- copydotfiles.sh
+++ copydotfiles.sh
@@ -5,15 +5,21 @@ echo "in vostok, write v. others, not work."
 echo "Write f, and adapt FISH."
 read g
 
+#dfpath=$HOME/dotfiles          #Ubuntu
+ dfpath=$HOME/develop/dotfiles  #Mac
+
+
 case $g in
-	"y" ) { cp ~/.vimrc ~/dotfiles/.vimrc
-		cp ~/.bash_profile ~/dotfiles/.bash_profile
+	"y" ) { cp ~/.vimrc $dfpath/.vimrc
+		cp ~/.bash_profile $dfpath/.bash_profile
+    cp ~/.SpaceVim.d/init.toml $dfpath/init.toml 
 	};;
 	"v" ) { cp ~/.vimrc ~/dotfiles/.vimrc_vos ~/vimrc
 		ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 	};;
-  "f" ) { ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
-    ln -sf ~/dotfiles/.vimrc ~/.vimrc
+  "f" ) { ln -sf $dfpath/config.fish ~/.config/fish/config.fish
+    ln -sf $dfpath/.vimrc ~/.vimrc
+    ln -sf $dfpath/init.toml  ~/.SpaceVim.d/init.toml
   };;
 	* )	echo "stop"
 esac
