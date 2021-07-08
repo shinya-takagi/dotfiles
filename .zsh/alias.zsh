7aa878dcaf5f7467e727b319a5700647a3c06941
Shinya Takagi <shinya523@gmail.com>
2021-07-09 06:50:50 +0900

    add zsh files.

---
+++ .zsh/alias.zsh
@@ -0,0 +1,33 @@
+
+# -----------------------------
+# Alias
+# -----------------------------
+# グローバルエイリアス
+alias -g L='| less'
+alias -g H='| head'
+alias -g G='| grep'
+alias -g GI='| grep -ri'
+
+# Alias
+alias ls='ls --color=auto'
+alias lst='ls -ltr --color=auto'
+alias la='ls -la --color=auto'
+alias ll='ls -l --color=auto'
+
+alias du="du -Th"
+alias df="df -Th"
+alias su="su -l"
+alias so='source'
+alias vi='vim'
+alias vz='vim ~/.zshrc'
+alias c='cd'
+alias cp='cp -i'
+alias rm='rm -i'
+alias mkdir='mkdir -p'
+alias ..='c ../'
+alias back='pushd'
+alias diff='diff -U1'
+#
+#alias tma='tmux attach'
+#alias tml='tmux list-window'
+
