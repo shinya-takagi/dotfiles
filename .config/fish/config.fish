5dbabcc5cc147f01ce2ab9b1ad393f7363b3f93d
shinya-takagi <shinya523@gmail.com>
2022-03-05 22:10:27 +0900

    Add fish completions and more.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,55 @@
+#
+# Fish configure
+#
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias vi='vim'
+# -----------------------------
+# Alias
+# -----------------------------
+# local Alias
+alias ls='ls --color=auto'
+alias lst='ls -ltr'
+alias la='ls -la'
+alias ll='ls -l'
+alias ltr='ll -rt'
+alias lh='ls -lh'
 
-set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
+#------git------------
+alias gs='git status'
+alias ga='git add'
+alias gb='git branch'
+alias gco='git checkout'
+alias gcm='git commit -a -m'
+alias gp='git push'
+alias gpm='gp origin master'
+
+# Vim
+alias vi='vim'
+alias vr='vi ~/.vimrc'
+
+# fish
+alias vf='vim ~/.config/fish/config.fish'
+alias sf='source ~/.config/fish/config.fish'
+
+# Set vim key-bind for fish
+fish_vi_key_bindings
+
+# environmental path
+# set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
+# Universal variables
+# set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin $fish_user_paths
+
+
+# SSH agent
+#set SSH_AGENT_FILE "$HOME/.ssh/ssh_agent"
+#test -f $SSH_AGENT_FILE; and source $SSH_AGENT_FILE > /dev/null 2>&1
+#if not ssh-add -l > /dev/null 2>&1
+#  if not ps -ef | grep -v grep | grep ssh-agent
+#    ssh-agent -c > $SSH_AGENT_FILE 2>&1
+#  end
+#  source $SSH_AGENT_FILE > /dev/null 2>&1
+#  find $HOME/.ssh -name id_rsa_vostok2 | xargs ssh-add
+#end
