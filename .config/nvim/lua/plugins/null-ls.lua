c442fcf60a36bfcccc3b511c14d07b3aba5eb152
shinya-takagi <shinya523@gmail.com>
2023-03-06 00:04:33 +0900

    Add: Fortran with formatter

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,12 +1,25 @@
 return {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function()
-      local nls = require("null-ls")
-      return {
-          sources = {
-              nls.builtins.formatting.black,
-              nls.builtins.formatting.stylua,
-          },
-      }
-    end,
+  "jose-elias-alvarez/null-ls.nvim",
+  opts = function()
+    local nls = require("null-ls")
+
+    -- Setting for disabling formatting Fixed Fortran.
+    local disabled_extensions_fortran = { "f", "for", "ftn" }
+    local fprettify = nls.builtins.formatting.fprettify.with({
+      runtime_condition = function(params)
+        local extension = vim.fn.fnamemodify(params.bufname, ":e")
+        return not vim.tbl_contains(disabled_extensions_fortran, extension)
+      end,
+    })
+    -- Fixed Fortran setting.
+
+    -- Load formatters
+    return {
+      sources = {
+        nls.builtins.formatting.black,
+        nls.builtins.formatting.stylua,
+        fprettify,
+      },
+    }
+  end,
 }
