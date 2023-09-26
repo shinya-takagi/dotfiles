571ce03bbbf53b7471e41006e0880fe61c53ef2a
shinya-takagi <shinya523@gmail.com>
2023-09-26 17:44:34 +0900

    Add: python virtualenv on statusline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-if not vim.g.vscode then
-  require("config.lazy")
-else
-  -- require("config.vscode")
-  require("config.lazy")
-  -- vim.opt.ignorecase = true
-  -- vim.opt.smartcase = true
-end
+-- if not vim.g.vscode then
+require("config.lazy")
+-- else
+-- require("config.lazy")
+-- end
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,21 @@
+local function split(input, delimiter)
+  local arr = {}
+  string.gsub(input, "[^" .. delimiter .. "]+", function(w)
+    table.insert(arr, w)
+  end)
+  return arr
+end
+
+local function get_venv()
+  local venv = vim.env.VIRTUAL_ENV
+  if venv then
+    local params = split(venv, "/")
+    return "(env:" .. params[table.getn(params) - 1] .. ")"
+  else
+    return ""
+  end
+end
+
 return {
   "nvim-lualine/lualine.nvim",
 
@@ -6,6 +24,9 @@ return {
       options = {
         -- themes = "monokai-pro",
         themes = "vscode",
+      },
+      sections = {
+        lualine_x = { { get_venv, color = { gui = "bold" } } },
       },
     }
   end,
