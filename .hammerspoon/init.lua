91931e825cea3b2a218aafbb2234cd6191e15f55
shinya-takagi <shinya523@gmail.com>
2023-12-01 18:23:54 +0900

    WIP Hammerspoon for Hotkey

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,12 +1,28 @@
 -- Setting for hammerspoon
+local spaces = require("hs._asm.undocumented.spaces")
+
+function MoveFullScreenWindow(app)
+	local activeSpace = spaces.activeSpace()
+	local win = app:focusedWindow()
+	-- Reset Fullscreen
+	win = win:toggleFullScreen()
+	win = win:toggleFullScreen()
+	app:hide()
+	win:spacesMoveTo(activeSpace)
+	win:focus()
+end
 
 hs.hotkey.bind({ "ctrl" }, "t", function()
 	local appname = "Alacritty"
 	local app = hs.application.get(appname)
 
-	if app == nil or app:isHidden() then
+	if app == nil then
 		hs.application.launchOrFocus(appname)
-	else
+	elseif app:isFrontmost() then
 		app:hide()
+	else
+		hs.application.launchOrFocus(appname)
+		-- MoveFullScreenWindow(app)
+		-- or app:isHidden() then
 	end
 end)
