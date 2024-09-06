7bba0789682de9b78fe88fc30a6c1b687da4b2e4
shinya-takagi <shinya523@gmail.com>
2024-09-06 18:09:27 +0900

    Comment out venv selector

--- .config/nvim/lua/plugins/venv.lua
+++ .config/nvim/lua/plugins/venv.lua
@@ -1,14 +1,14 @@
 return {
-  "linux-cultist/venv-selector.nvim",
-  cmd = "VenvSelect",
-  opts = {
-    name = {
-      "venv",
-      ".venv",
-      "env",
-      ".env",
-    },
-    dap_enabled = false, -- Ensure that the venv selector affect PythonPath in nvim-dap as well!
-  },
-  keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+  -- "linux-cultist/venv-selector.nvim",
+  -- cmd = "VenvSelect",
+  -- opts = {
+  --   name = {
+  --     "venv",
+  --     ".venv",
+  --     "env",
+  --     ".env",
+  --   },
+  --   dap_enabled = false, -- Ensure that the venv selector affect PythonPath in nvim-dap as well!
+  -- },
+  -- keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
 }
